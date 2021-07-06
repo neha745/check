07725fbe23af39abb7d6725322c8f542431c1888
neha745 <84764251+neha745@users.noreply.github.com>
2021-07-06 11:27:51 +0200

    Update demo.scala

--- demo.scala
+++ demo.scala
@@ -1,1 +1,5 @@
-
+package ScalaSparkCode
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, lit, when}
+import org.apache.spark.sql.types.StringType
